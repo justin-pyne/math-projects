42e89919bd09e944bbc836b2a1eb8ec00f85aa72
justin-pyne <jpyne.justin@gmail.com>
2023-10-31 16:14:03 -0600

    adding fit line to CLT

--- central_limit_theorem.py
+++ central_limit_theorem.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 def central_limit_theorem():
     np.random.seed(0)
@@ -12,10 +13,22 @@ def central_limit_theorem():
         sample_mean = np.mean(data)
         means.append(sample_mean)
 
+    # Plot histogram of sample means
     plt.hist(means, bins=30, density=True, color='blue', alpha=0.7)
+
+    # Fit normal distribution to sample means and plot
+    mu, std = norm.fit(means)
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std)
+    plt.plot(x, p, 'k', linewidth=2, label="Fit")
+
+    title = "Central Limit Theorem\nFit results: mu = %.2f,  std = %.2f" % (mu, std)
     plt.xlabel("Sample Mean")
     plt.ylabel("Frequency")
-    plt.title("Central Limit Theorem")
+    plt.title(title)
+    plt.legend()
     plt.show()
 
 central_limit_theorem()
+
