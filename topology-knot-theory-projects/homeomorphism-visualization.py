6792d77cbdeff12e9da95124c2d0d42434a7ae66
justin-pyne <jpyne.justin@gmail.com>
2023-11-03 15:36:13 -0600

    formatting

--- topology-knot-theory-projects/homeomorphism-visualization.py
+++ topology-knot-theory-projects/homeomorphism-visualization.py
@@ -1,9 +1,9 @@
 import plotly.graph_objects as go
 import numpy as np
 
-# 1. Generate 10,000 points on a sphere
-phi = np.linspace(0, np.pi, 100)  # Co-latitude
-theta = np.linspace(0, 2 * np.pi, 100)  # Longitude
+# sphere
+phi = np.linspace(0, np.pi, 100)  # co-latitude
+theta = np.linspace(0, 2 * np.pi, 100)  # longitude
 phi, theta = np.meshgrid(phi, theta)
 phi, theta = phi.flatten(), theta.flatten()
 
@@ -11,30 +11,30 @@ x = np.sin(phi) * np.cos(theta)
 y = np.sin(phi) * np.sin(theta)
 z = np.cos(phi)
 
-# 2. Project the points from the sphere to the cube
+# 2. project sphere to cube
 def sphere_to_cube(x, y, z):
     max_axis = np.max([abs(x), abs(y), abs(z)])
     return x/max_axis, y/max_axis, z/max_axis
 
 cube_x, cube_y, cube_z = np.vectorize(sphere_to_cube)(x, y, z)
 
-# 3. & 4. Morphing and plotting
+# morphing and plotting
 def morph_shapes(alpha, s_x, s_y, s_z, c_x, c_y, c_z):
     morphed_x = (1-alpha)*np.array(c_x) + alpha*np.array(s_x)
     morphed_y = (1-alpha)*np.array(c_y) + alpha*np.array(s_y)
     morphed_z = (1-alpha)*np.array(c_z) + alpha*np.array(s_z)
     return morphed_x, morphed_y, morphed_z
 
-# Initial plot (cube)
+# init plot (cube)
 m_x, m_y, m_z = morph_shapes(0, x, y, z, cube_x, cube_y, cube_z)
 scatter = go.Scatter3d(x=m_x, y=m_y, z=m_z, mode='markers', marker=dict(size=2))
 
-# Create figure
+# create figure
 fig = go.Figure(data=[scatter])
 fig.update_layout(title='Morphing Cube to Sphere', autosize=False, 
                   width=500, height=500, margin=dict(l=65, r=50, b=65, t=90))
 
-# Add slider to adjust morphing
+# slider
 steps = []
 for step in np.linspace(0, 1, 100):
     m_x, m_y, m_z = morph_shapes(step, x, y, z, cube_x, cube_y, cube_z)
